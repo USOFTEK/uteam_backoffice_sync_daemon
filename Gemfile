49688ce6514d3fe017ef1b89a6fb8c2a1e3c5cfc
Yaro Holodiuk <numberonename@gmail.com>
2014-10-28 16:16:34 +0200

    [~] Prepare for deploy

--- Gemfile
+++ Gemfile
@@ -1,5 +1,12 @@
 #add gems from tracker
-eval(IO.read("#{File.expand_path(File.join(File.dirname(__FILE__), "..", "goliath", "tracker"))}/Gemfile"), binding)
+begin
+  dirname = File.dirname(__FILE__)
+  gemfile = dirname.include?("/var/www/cabinet") ? "/var/www/cabinet/tracker/current/Gemfile" :
+      File.join(dirname, "..", "Meta", "cabina", "tracker", "Gemfile")
+  eval(IO.read(gemfile), binding)
+rescue Errno::ENOENT
+  p "Tracker gemfile can't be loaded"
+end
 
 # Mysql2
 gem "mysql2"
--- config/config.rb
+++ config/config.rb
@@ -2,8 +2,23 @@
 #activerecord
 I18n.enforce_available_locales = false
 
+ENVIR ||= "#{ ENV['ENVIR'] || :development }".downcase.to_sym
+p ENVIR
+
+settings = {
+    development: {
+        path_to_tracker: File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "Meta", "cabina", "tracker")),
+        sql_user: "root",
+        sql_pass: "123456789"
+    },
+    staging: {
+        path_to_tracker: "/var/www/cabinet/tracker/current",
+        sql_user: "root",
+        sql_pass: "123456789"
+    }
+}
 #paths
-PATH_TO_TRACKER = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "Meta", "cabina", "tracker"))
+PATH_TO_TRACKER = settings[ENVIR][:path_to_tracker]
 PATH_TO_TRACKER_MODELS = "#{PATH_TO_TRACKER}/app/models/*.rb"
 PATH_TO_TRACKER_DB_YAML = "#{PATH_TO_TRACKER}/config/database.yml"
 
@@ -13,8 +28,8 @@ PATH_TO_CURRENT_DB_YAML = "#{File.dirname(__FILE__)}/database_cb.yml"
 DUMP_CURRENT_DB_NAME = "billing_track_development"
 
 #credentials
-SQL_USER = "root"
-SQL_PASS = "123456789"
+SQL_USER = settings[ENVIR][:sql_user]
+SQL_PASS = settings[ENVIR][:sql_pass]
 
 
 # program logic
